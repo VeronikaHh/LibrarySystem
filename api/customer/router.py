d9bab5067663e7567b1b4576f4f1848b9e8d9ccb
Veronika Holovanova <golovanovaveronika@gmail.com>
2024-12-19 11:44:56 +0200

    added book router

--- api/customer/router.py
+++ api/customer/router.py
@@ -11,8 +11,3 @@ router = APIRouter()
 # @router.get("/users/me", tags=["users"])
 # async def read_user_me():
 #     return {"username": "fakecurrentuser"}
-#
-#
-# @router.get("/users/{username}", tags=["users"])
-# async def read_user(username: str):
-#     return {"username": username}
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
+
 from log_config import logger
+
 from api.book.router import router as book_router
 from api.customer.router import router as customer_router
 from api.employee.router import router as employee_router
@@ -12,10 +14,6 @@ app.include_router(book_router)
 app.include_router(customer_router)
 app.include_router(employee_router)
 app.include_router(order_router)
-
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
 
 if __name__ == "__main__":
     logger.debug("Starting app")
