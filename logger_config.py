0e9b1bc815093548bb00add7cff54b524563361f
Veronika Holovanova <74206053+VeronikaHh@users.noreply.github.com>
2024-12-24 14:24:27 +0200

    Changed logging configs, added logging format (#13)
    * Changed logging configs, and added logging format

---
+++ logger_config.py
@@ -0,0 +1,11 @@
+import logging
+
+from config import system_config, DATETIME_FORMAT, LOGGING_FORMAT
+
+logging.basicConfig(
+    level=system_config.log_level,
+    format=LOGGING_FORMAT,
+    datefmt=DATETIME_FORMAT,
+)
+
+logger = logging.getLogger()
