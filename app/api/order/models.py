c574221b8900c784d367f0cd617ba71f260cd6e9
Veronika Holovanova <golovanovaveronika@gmail.com>
2024-12-30 20:10:40 +0200

    Added migration for unique constraints

--- app/api/order/models.py
+++ app/api/order/models.py
@@ -6,13 +6,15 @@ from sqlmodel import Field, SQLModel
 
 class OrderUpdate(SQLModel):
     end_date: datetime | None = None
-    is_returned: bool | None= None
+    is_returned: bool | None = None
 
 
-class OrderCreate(OrderUpdate):
+class OrderCreate(SQLModel):
     customer_id: uuid.UUID = Field(foreign_key="customers.customer_id")
     book_id: uuid.UUID = Field(foreign_key="books.book_id")
     employee_id: uuid.UUID = Field(foreign_key="employees.employee_id")
+    end_date: datetime = Field(default_factory=lambda: datetime.now(timezone.utc) + timedelta(days=90))
+    is_returned: bool = Field(default=False)
 
 
 class Order(OrderCreate, table=True):
