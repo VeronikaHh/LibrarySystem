dec8da469bbd0cb01756afac7b2d61cefd739a29
Veronika Holovanova <golovanovaveronika@gmail.com>
2025-01-05 15:10:02 +0200

    Added tests for order router

---
+++ tests/api/order/test_routers.py
@@ -0,0 +1,52 @@
+import uuid
+
+from fastapi.testclient import TestClient
+from fastapi.encoders import jsonable_encoder
+
+from app.api.order import Order, OrderCreate
+
+
+def test_get_all_orders(test_client: TestClient) -> None:
+    response = test_client.get("/orders")
+    assert response.status_code == 200
+
+
+def test_get_order_by_id(test_client: TestClient, orders: list[Order]) -> None:
+    order_id = orders[0].order_id
+    response = test_client.get(f"/orders/{order_id}")
+    assert response.status_code == 200
+
+
+def test_get_order_by_id_not_exist(test_client: TestClient) -> None:
+    order_id = uuid.uuid4()
+    response = test_client.get(f"/orders/{order_id}")
+    assert response.status_code == 404
+
+
+def test_create_order(test_client: TestClient, create_order_request: OrderCreate) -> None:
+    response = test_client.post("/orders", json=jsonable_encoder(create_order_request))
+    assert response.status_code == 201
+
+
+def test_update_order(test_client: TestClient, orders: list[Order]) -> None:
+    order_id = orders[0].order_id
+    response = test_client.put(f"/orders/{order_id}", json={})
+    assert response.status_code == 200
+
+
+def test_update_order_not_exist(test_client: TestClient) -> None:
+    order_id = uuid.uuid4()
+    response = test_client.put(f"/orders/{order_id}", json={})
+    assert response.status_code == 404
+
+
+def test_delete_order(test_client: TestClient, orders: list[Order]) -> None:
+    order_id = orders[0].order_id
+    response = test_client.delete(f"/orders/{order_id}")
+    assert response.status_code == 204
+
+
+def test_delete_order_not_exist(test_client: TestClient) -> None:
+    order_id = uuid.uuid4()
+    response = test_client.delete(f"/orders/{order_id}")
+    assert response.status_code == 404
---
+++ tests/fixtures/order.py
@@ -0,0 +1,48 @@
+import pytest
+from sqlmodel import Session
+
+from app.api.order import Order, OrderCreate, OrderDataAccessLayer
+from app.api.book import Book
+from app.api.customer import Customer
+from app.api.employee import Employee
+
+
+@pytest.fixture(scope="session")
+def orders_dal(
+        test_database_session: Session,
+) -> OrderDataAccessLayer:
+    return OrderDataAccessLayer(
+        session=test_database_session,
+    )
+
+
+@pytest.fixture(scope="module")
+def orders(
+        orders_dal: OrderDataAccessLayer,
+        books: list[Book],
+        customers: list[Customer],
+        employees: list[Employee],
+) -> list[Order]:
+    sample_orders = [
+        Order(
+            customer_id=customers[i].customer_id,
+            book_id=books[i].book_id,
+            employee_id=employees[i].employee_id,
+        ) for i in range(2)
+    ]
+    for order in sample_orders:
+        orders_dal.create_order(order)
+    return sample_orders
+
+
+@pytest.fixture(scope="module")
+def create_order_request(
+        books: list[Book],
+        customers: list[Customer],
+        employees: list[Employee],
+) -> OrderCreate:
+    return OrderCreate(
+        customer_id=customers[0].customer_id,
+        book_id=books[1].book_id,
+        employee_id=employees[0].employee_id,
+    )
